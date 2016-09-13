8cd32ee64806cc0f2457c4b3a2867f9ea911e9ff
Selvakumar Velusamy <nvselva@gmail.com>
2016-09-12 18:11:41 -0700

    Initial Checkin

---
+++ src/main/java/com/boeing/hams/dto/App.java
@@ -0,0 +1,13 @@
+package com.boeing.hams.dto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
